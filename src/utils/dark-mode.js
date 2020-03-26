66cf2449da50779f6a34c78f25dbc396915848eb
pofay <gianCarlo.gilos@gmail.com>
2020-03-26 15:33:53 +0800

    Configuring prismjs for code highlighting

--- src/utils/dark-mode.js
+++ src/utils/dark-mode.js
@@ -4,23 +4,21 @@ import Brightness7Icon from "@material-ui/icons/Brightness7"
 import Brightness4Icon from "@material-ui/icons/Brightness4"
 import React from "react"
 
-const DarkMode = props => {
-  return (
-    <div style={{ height: 70 }}>
-      <ThemeToggler>
-        {({ theme, toggleTheme }) => (
-          <IconButton
-            aria-label="toggle-theme"
-            onClick={e => {
-              theme === "dark" ? toggleTheme("light") : toggleTheme("dark")
-            }}
-          >
-            {theme === "dark" ? <Brightness7Icon /> : <Brightness4Icon />}
-          </IconButton>
-        )}
-      </ThemeToggler>
-    </div>
-  )
-}
+const DarkMode = props => (
+  <div style={{ height: 70 }}>
+    <ThemeToggler>
+      {({ theme, toggleTheme }) => (
+        <IconButton
+          aria-label="toggle-theme"
+          onClick={e => {
+            theme === "dark" ? toggleTheme("light") : toggleTheme("dark")
+          }}
+        >
+          {theme === "dark" ? <Brightness7Icon /> : <Brightness4Icon />}
+        </IconButton>
+      )}
+    </ThemeToggler>
+  </div>
+)
 
 export default DarkMode
